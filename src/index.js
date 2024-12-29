aa1862e569bd6e4e082f17eb8e6f7956b3f67d16
Soumen Ghorai <soumenghorai2802@gmail.com>
2024-12-29 23:39:19 +0530

    added socket.io setup

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import express from 'express';
+import { createServer } from 'http';
 import { StatusCodes } from 'http-status-codes';
+import { Server } from 'socket.io';
 
 import bullServerAdapter from './config/bullBoardConfig.js';
 import connectDB from './config/dbConfig.js';
@@ -7,6 +9,8 @@ import { PORT } from './config/serverConfig.js';
 import apiRouter from './routes/apiRoutes.js';
 
 const app = express();
+const server = createServer(app);
+const io = new Server(server);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -19,7 +23,17 @@ app.get('/ping', (req, res) => {
   return res.status(StatusCodes.OK).json({ message: 'Ping' });
 });
 
-app.listen(PORT, async () => {
+io.on('connection', (socket) => {
+  console.log('a user connected', socket.id);
+
+  socket.on('messageFromClient', (data) => {
+    console.log('message from client', data);
+
+    io.emit('new message', data.toUpperCase());
+  });
+});
+
+server.listen(PORT, async () => {
   console.log(`Server is running on port ${PORT}`);
   connectDB();
 });
--- src/routes/v1/messages.js
+++ src/routes/v1/messages.js
@@ -1,7 +1,7 @@
 import express from 'express';
 
-import { isAuthenticated } from '../../middlewares/authMiddleware.js';
 import { getMessages } from '../../controllers/messageController.js';
+import { isAuthenticated } from '../../middlewares/authMiddleware.js';
 
 const router = express.Router();
 
