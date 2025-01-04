4dd1d36581d3993970ea736b47856f1348be78ac
Soumen Ghorai <soumenghorai2802@gmail.com>
2025-01-05 02:11:24 +0530

    added workspace sidebar

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import cors from 'cors';
 import express from 'express';
 import { createServer } from 'http';
 import { StatusCodes } from 'http-status-codes';
@@ -12,7 +13,13 @@ import apiRouter from './routes/apiRoutes.js';
 
 const app = express();
 const server = createServer(app);
-const io = new Server(server);
+const io = new Server(server, {
+  cors: {
+    origin: '*'
+  }
+});
+
+app.use(cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
